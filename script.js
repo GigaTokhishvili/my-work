9f95e32fa51a4ab674b8e7c33921111abe7051c0
Giga Tokhishvili <toxishvili123@gmail.com>
2023-02-18 17:19:55 +0400

    improved rotation
    finally managed to make it remember previous rotations

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const disk = document.querySelector('.disk');
 
 let isDown = false;
+let startX = 0;
+let startY = 0;
+let initialAngle = 0;
 let previousAngle = 0;
 let angle = 0;
 let currentX 
@@ -14,6 +17,10 @@ if (window.matchMedia('(max-width: 450px)').matches) {
     disk.addEventListener("touchstart", (e) => {
         e.preventDefault();
         isDown = true;
+
+        currentX = e.touches[0].clientX - disk.offsetLeft - 150;
+        currentY = e.touches[0].clientY - disk.offsetTop - 150;
+        initialAngle = 180 + Math.atan2(currentY, currentX) * 180 / Math.PI;
     });
   
     document.body.addEventListener("touchmove", (e) => {
@@ -23,19 +30,27 @@ if (window.matchMedia('(max-width: 450px)').matches) {
         currentX = e.touches[0].clientX - disk.offsetLeft - 150;
         currentY = e.touches[0].clientY - disk.offsetTop - 150;
   
-        angle = (Math.atan2(currentY, currentX) * 180) / Math.PI;
-        disk.style.transform = `rotate(${angle}deg)`;
+        angle = 180 + (Math.atan2(currentY, currentX) * 180) / Math.PI;
+        disk.style.transform = `rotate(${previousAngle + (angle - initialAngle)}deg)`;
         }
     });
 
     document.body.addEventListener("touchend", () => {
         isDown = false;
-        previousAngle = angle;
+        previousAngle += angle - initialAngle;
     });
 } else {
+
+
+
+    // desktop version
     disk.addEventListener('mousedown', (e) => {
         e.preventDefault;
-        isDown = true;    
+        isDown = true;
+        currentX = e.clientX - disk.offsetLeft - center;
+        currentY = e.clientY - disk.offsetTop - center;
+        initialAngle = 180 + Math.atan2(currentY, currentX) * 180 / Math.PI;
+        
     })
 
     document.body.addEventListener('mousemove', (e) => {
@@ -44,17 +59,15 @@ if (window.matchMedia('(max-width: 450px)').matches) {
         if (e.buttons === 1) {
             currentX = e.clientX - disk.offsetLeft - center;
             currentY = e.clientY - disk.offsetTop - center;
-        
-            angle = Math.atan2(currentY, currentX) * 180 / Math.PI;
-            averageAngle = angle;
-            disk.style.transform =`rotate(${averageAngle}deg)`;
-            console.log(angle, averageAngle);
+
+            angle = 180 + Math.atan2(currentY, currentX) * 180 / Math.PI;
+            disk.style.transform =`rotate(${previousAngle + (angle - initialAngle)}deg)`;
         }
     })
 
     document.body.addEventListener('mouseup', () => {
         isDown = false;
-        previousAngle = angle;
+        previousAngle += angle - initialAngle;
     })
 }
 
