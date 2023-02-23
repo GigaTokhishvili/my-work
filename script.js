f56cbc6cfcc6fc8a4beba45337c3745f2e486db8
GigaTokhishvili <toxishvili123@gmail.com>
2023-02-23 13:19:40 +0400

    animation
    added more animations

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-//The Rotation
-const disk = document.querySelector('disk');
-const items = document.querySelectorAll('.disk');
-
-
-const tl = gsap.timeline({defaults: {duration: 1.25}});
-tl.fromTo(items, {scale: 0}, {scale: 1, rotation: '360deg'});
-
-Draggable.create ('.disk', {type:'rotation'});
-
-
-
 // contact me 
 const emailButton = document.getElementById('emailButton');
 const emailForm = document.querySelector('.emailForm');
@@ -21,3 +9,23 @@ emailButton.addEventListener('click', ()=> {
 closeButton.addEventListener('click', ()=> {
     emailForm.classList.remove('visible');
 })
+
+
+//The Rotation and animations
+const disk = document.querySelector('disk');
+const items = document.querySelectorAll('.disk');
+const mainText = document.getElementById('mainText');
+const mainCode = document.getElementById('mainCode');
+
+
+const tl = gsap.timeline({defaults: {duration: 1.25}});
+tl.fromTo(items, {scale: 0}, {scale: 1, rotation: '360deg'});
+tl.fromTo(mainText, {opacity: 0, x: 300}, {opacity: 1, x: 0}, '<50%');
+tl.fromTo(emailButton, {opacity: 0, y: -50}, {opacity: 1, y: 0}, '<50%');
+tl.fromTo(mainCode, {opacity: 0, y: -50}, {opacity: 1, y: 0}, '<35%');
+
+
+Draggable.create ('.disk', {type:'rotation'});
+
+
+
